d33076482c65c62f3aa1146a0013bf43b6cd85f1
surkanghyun <qorrnfn2000@naver.com>
2024-04-11 11:59:06 +0900

    카카오로그인수정본

--- src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
+++ src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
@@ -1,51 +1,111 @@
 package com.spring.mvc.chap05.service;
 
+import com.spring.mvc.chap05.dto.request.SignUpRequestDTO;
+import com.spring.mvc.chap05.dto.response.KakaoUserResponseDTO;
+import com.spring.mvc.chap05.entity.Member;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestParam ;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-@Controller
+@Service
 @Slf4j
 @RequiredArgsConstructor
 public class SnsLoginService {
-    public void kakaoLogin(Map<String,String> params) {
 
-        getKakaoAccessToken(params);
+    private final MemberService memberService;
+
+    // 카카오 로그인 처리
+    public void kakaoLogin(Map<String, String> params, HttpSession session) {
+
+        String accessToken = getKakaoAccessToken(params);
+        log.info("access_token: {}", accessToken);
+
+        // 전달받은 액세스 토큰을 활용하여 사용자 정보를 가져오기.
+        KakaoUserResponseDTO dto = getKakaoUserInfo(accessToken);
+
+        // 카카오에서 받은 회원정보로 우리 사이트 회원가입
+        String email = dto.getAccount().getEmail();
+        log.info("이메일: {}", email);
+
+        // 회원 중복확인 (이메일)
+        if (!memberService.checkDuplicateValue("email", email)) {
+            // 한번도 카카오 로그인을 한 적이 없다면 회원가입이 들어간다.
+            memberService.join(
+                    SignUpRequestDTO.builder()
+                            .account(String.valueOf(dto.getId()))
+                            .password("0000")
+                            .name(dto.getProperties().getNickname())
+                            .email(email)
+                            .loginMethod(Member.LoginMethod.KAKAO)
+                            .build(),
+                    dto.getProperties().getProfileImage()
+            );
+        }
+
+        // 우리 사이트 로그인 처리
+        memberService.maintainLoginState(session, String.valueOf(dto.getId()));
+
+
     }
 
+    private KakaoUserResponseDTO getKakaoUserInfo(String accessToken) {
 
-    //토큰 발급 요청
+        String requestUri = "https://kapi.kakao.com/v2/user/me";
+
+        // 요청 헤더
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 요청 보내기
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<KakaoUserResponseDTO> responseEntity = template.exchange(
+                requestUri,
+                HttpMethod.POST,
+                new HttpEntity<>(headers),
+                KakaoUserResponseDTO.class
+        );
+
+        KakaoUserResponseDTO responseJSON = responseEntity.getBody();
+        log.info("응답 데이터 결과: {}", responseJSON);
+
+        return responseJSON;
+    }
+
+    // 토큰 발급 요청
     private String getKakaoAccessToken(Map<String, String> requestParam) {
-        //요청 URI
+
+        // 요청 URI
         String requestUri = "https://kauth.kakao.com/oauth/token";
 
-        //요청 헤더 설정
+        // 요청 헤더 설정
         HttpHeaders headers = new HttpHeaders();
-        headers.add("content-type","application/x-www-form-urlencoded;charset=utf-8");
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        //요청 바디에 파라미터 세팅
+        // 요청 바디에 파라미터 세팅
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type" ,"authorization_code");
-        params.add("client_id", requestParam.get("addKey"));
-        params.add("redirect_rui", requestParam.get("redirect"));
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", requestParam.get("appKey"));
+        params.add("redirect_uri", requestParam.get("redirect"));
         params.add("code", requestParam.get("code"));
 
-        //카카오 인증서버로 POST 요청 날리기
+        // 카카오 인증서버로 POST 요청 날리기
         RestTemplate template = new RestTemplate();
 
-        // 헤더 정보와 파라미터를 하나로 묶기
-        HttpEntity<Object> requestEntity = new HttpEntity<>(params , headers);
-  /*
+        // 헤더 정보와 파라미터를 하나로 묶기.
+        HttpEntity<Object> requestEntity = new HttpEntity<>(params, headers);
+
+        /*
             - RestTemplate객체가 REST API 통신을 위한 API인데 (자바스크립트 fetch역할)
             - 서버에 통신을 보내면서 응답을 받을 수 있는 메서드가 exchange
             param1: 요청 URL
@@ -56,11 +116,11 @@ public class SnsLoginService {
         ResponseEntity<Map> responseEntity
                 = template.exchange(requestUri, HttpMethod.POST, requestEntity, Map.class);
 
-        //응답 데이터 에서 JSON 추출
-       Map<String,Object> responseJSON = (Map<String,Object>) responseEntity.getBody();
-       log.info("응답 JSON 데이터: {}" , responseJSON);
+        // 응답 데이터에서 JSON 추출
+        Map<String, Object> responseJSON = (Map<String, Object>) responseEntity.getBody();
+        log.info("응답 JSON 데이터: {}", responseJSON);
 
-       //access token 추출 (카카오 로그인 중인 사용자의 정보를 요청할때 필요한 토큰)
+        // access token 추출 (카카오 로그인 중인 사용자의 정보를 요청할 때 필요한 토큰)
         String accessToken = (String) responseJSON.get("access_token");
 
         return accessToken;
